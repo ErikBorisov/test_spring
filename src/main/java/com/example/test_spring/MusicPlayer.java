76b39991febdc4ac6ad987d30b378cae83344d9c
ErikBorisov <Erik.borisov.2099@gmail.com>
2022-09-30 10:50:02 +0400

    List<Music> example

--- src/main/java/com/example/test_spring/MusicPlayer.java
+++ src/main/java/com/example/test_spring/MusicPlayer.java
@@ -1,7 +1,10 @@
 package com.example.test_spring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> music = new ArrayList<>();
 
     private String name;
     private int volume;
@@ -23,14 +26,29 @@ public class MusicPlayer {
     }
 
     public MusicPlayer(){}
+
+    public MusicPlayer(List<Music> music) {
+        this.music = music;
+    }
+
+    public List<Music> getMusic() {
+        return music;
+    }
+
+    public void setMusic(List<Music> music) {
+        this.music = music;
+    }
+
     //IoC
-    public MusicPlayer(Music music){
-        this.music = music;
-    }
-    public void setMusic(Music music){
-        this.music = music;
-    }
+//    public MusicPlayer(Music music){
+//        this.music = music;
+//    }
+//    public void setMusic(Music music){
+//        this.music = music;
+//    }
     public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+        for (Music music1 : music) {
+            System.out.println("Playing: " + music1.getSong());
+        }
     }
 }
