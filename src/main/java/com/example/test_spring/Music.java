926fd010ac2a6ac51bc84d70f1dd4f3711f6e8ea
ErikBorisov <Erik.borisov.2099@gmail.com>
2022-09-30 10:18:09 +0400

    Initial commit

---
+++ src/main/java/com/example/test_spring/Music.java
@@ -0,0 +1,5 @@
+package com.example.test_spring;
+
+public interface Music {
+    String getSong();
+}
---
+++ src/main/java/com/example/test_spring/RockMusic.java
@@ -0,0 +1,8 @@
+package com.example.test_spring;
+
+public class RockMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Wind cries Mary";
+    }
+}
